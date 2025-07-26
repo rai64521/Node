8cfb0c07a00fe0b5aa8d0ba728b81ab1f1a928b0
rai64521 <raisumant99@gmail.com>
2025-07-26 10:34:17 +0530

    path module in node js

--- FileModule/index.js
+++ FileModule/index.js
@@ -14,5 +14,18 @@ fs.readFile("./contact.txt","utf-8",(err,result)=>{
     
 })
 
- 
+
+fs.appendFileSync("./append.txt",`\n I want to append Date: ${Date.now()}`)
+
+fs.appendFile("./appennd.txt",`\n i want to append async Date: ${Date.now()}`,(err)=>{
+
+})
+
+// if you want to copy the file one to another then use copy in nodejs
+
+fs.copyFileSync("./rai.txt","./copy.txt" )
+
+// If you want to delete the file use unlink to delete 
+
+
 
